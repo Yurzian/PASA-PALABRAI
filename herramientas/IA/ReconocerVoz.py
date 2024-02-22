0890ade3be5f314a0c1bec2a3ebcfba17cdf930e
Daniel García Pérez <95293092+Yurzian@users.noreply.github.com>
2024-02-22 14:53:16 +0100

    Update ReconocerVoz.py

--- herramientas/IA/ReconocerVoz.py
+++ herramientas/IA/ReconocerVoz.py
@@ -12,22 +12,20 @@ class ReconocerVoz:
 
     def reconocer_voz(self):
         """
-                Función para reconocer voz a través del micrófono.
-
-                Utiliza el micrófono como fuente de audio para escuchar al usuario.
-                Utiliza el reconocedor de voz de Google para convertir el audio a texto.
-
-                Returns:
-                - str: El texto reconocido a partir del audio.
-                """
-        while True:
+        Función para reconocer voz a través del micrófono.
+        Utiliza el micrófono como fuente de audio para escuchar al usuario.
+        Utiliza el reconocedor de voz de Google para convertir el audio a texto.
+    
+        Returns:
+        - str: El texto reconocido a partir del audio, o None si no se reconoce nada.
+        """
+        intento = 0
+        while intento < 2:
             try:
-                # Usa el micrófono como fuente de audio
                 with sr.Microphone() as source:
-                    # Escucha el audio a través del micrófono
-                    audio = self.r.listen(source, timeout=5)  # Se establece un tiempo límite de 5 segundos
-
-                    # Usa el reconocedor de voz de Google para convertir el audio a texto
+                    print("Escuchando...")
+                    audio = self.r.listen(source, timeout=5)
+                    print("Procesando audio...")
                     text = self.r.recognize_google(audio, language='es-ES')
                     if text:
                         return text
@@ -35,5 +33,8 @@ class ReconocerVoz:
                 print("Google Speech Recognition no pudo entender el audio, intenta de nuevo")
             except sr.RequestError as e:
                 print("No se pudo solicitar resultados a Google Speech Recognition; {0}".format(e))
-            except sr.WaitTimeoutError:  # Agregar una excepción para el tiempo de espera
+            except sr.WaitTimeoutError:
                 print("Tiempo de espera agotado. No se detectó audio.")
+            intento += 1
+        print("Número máximo de intentos alcanzado. No se pudo reconocer el audio.")
+        return ""
